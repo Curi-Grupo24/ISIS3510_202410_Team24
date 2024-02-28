5c57e6cdbccfaf0abb49d364c57b1ded33acdaf3
Diana Alejandra Silva Alvarez <da.silvaa@uniandes.edu.co>
2024-02-28 15:37:32 -0500

    FEAT: added route to login

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,13 +1,14 @@
 import 'package:get/route_manager.dart';
 
+import '../../features/auth/presentation/pages/pages.dart';
+
 List<GetPage<dynamic>> get getRoutes {
   const Duration transitionDuration = Duration(milliseconds: 350);
   return <GetPage<dynamic>>[
-    // GetPage<dynamic>(
-    //   name: '/home',
-    //   page: HomePage.new,
-    //   middlewares: <GetMiddleware>[GetMiddleware()],
-    // ),
-    
+    GetPage<dynamic>(
+      name: '/login',
+      page: Login.new,
+      middlewares: <GetMiddleware>[GetMiddleware()],
+    ),
   ];
 }

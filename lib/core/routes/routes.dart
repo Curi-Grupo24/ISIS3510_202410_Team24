f3153c8b81b21e7c1d91ca887bdeafe438ea1585
Diana Alejandra Silva Alvarez <da.silvaa@uniandes.edu.co>
2024-02-28 10:34:56 -0500

    FEAT: added lintern, constants and pages

---
+++ lib/core/routes/routes.dart
@@ -0,0 +1,13 @@
+import 'package:get/route_manager.dart';
+
+List<GetPage<dynamic>> get getRoutes {
+  const Duration transitionDuration = Duration(milliseconds: 350);
+  return <GetPage<dynamic>>[
+    // GetPage<dynamic>(
+    //   name: '/home',
+    //   page: HomePage.new,
+    //   middlewares: <GetMiddleware>[GetMiddleware()],
+    // ),
+    
+  ];
+}
---
+++ lib/core/ui/colors_brand.dart
@@ -0,0 +1,51 @@
+import 'dart:ui';
+
+class Colors {
+  static const Map<int, Color> purpleBrand = <int, Color>{
+    20: Color(0xff553ee8),
+    30: Color(0xFF6936DD),
+    50: Color(0xff4A27D4),
+    60: Color(0xFF481ABB),
+  };
+
+  static const Map<int, Color> bitterSweet = <int, Color>{
+    0: Color.fromRGBO(255, 221, 221, 1),
+    5: Color.fromRGBO(255, 182, 182, 1),
+    10: Color.fromRGBO(255, 108, 108, 1),
+    20: Color.fromRGBO(255, 70, 70, 1),
+    30: Color.fromRGBO(208, 29, 29, 1),
+    40: Color.fromRGBO(172, 13, 13, 1),
+    50: Color.fromRGBO(143, 8, 8, 1),
+  };
+  static const Map<int, Color> gray = <int, Color>{
+    0: Color.fromRGBO(255, 255, 255, 1),
+    10: Color.fromRGBO(251, 252, 253, 1),
+    20: Color.fromRGBO(245, 248, 250, 1),
+    30: Color.fromRGBO(239, 243, 249, 1),
+    40: Color.fromRGBO(221, 224, 232, 1),
+    50: Color.fromRGBO(153, 172, 194, 1),
+    60: Color.fromRGBO(122, 147, 171, 1),
+    70: Color.fromRGBO(78, 108, 136, 1),
+    80: Color.fromRGBO(44, 67, 89, 1),
+    90: Color.fromRGBO(32, 45, 57, 1),
+  };
+  static const Map<int, Color> neutralGray = <int, Color>{
+    0: Color.fromRGBO(231, 231, 231, 1),
+    10: Color.fromRGBO(208, 208, 208, 1),
+    20: Color.fromRGBO(184, 184, 184, 1),
+    30: Color.fromRGBO(160, 160, 160, 1),
+    40: Color.fromRGBO(137, 137, 137, 1),
+    50: Color.fromRGBO(113, 113, 113, 1),
+    60: Color.fromRGBO(89, 89, 89, 1),
+    70: Color.fromRGBO(65, 65, 65, 1),
+    80: Color.fromRGBO(42, 42, 42, 1),
+    90: Color.fromRGBO(18, 18, 18, 1),
+  };
+  static const Map<int, Color> white = <int, Color>{
+    0: Color.fromRGBO(255, 255, 255, 1),
+  };
+  static const Map<int, Color> black = <int, Color>{
+    0: Color.fromRGBO(0, 0, 0, 1),
+  };
+  static const Color transparent = Color.fromRGBO(255, 255, 255, 0);
+}

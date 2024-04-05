3651778360029d57cf8275d7b572852b37f6070a
Diana Alejandra Silva Alvarez <da.silvaa@uniandes.edu.co>
2024-04-05 10:19:49 -0500

    FEAT: added validation to login to show error message

--- lib/features/auth/presentation/pages/login.dart
+++ lib/features/auth/presentation/pages/login.dart
@@ -42,127 +42,139 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: BlocBuilder<LoginBloc, LoginState>(
-          bloc: loginBloc,
-          builder: (BuildContext context, LoginState state) {
-            if (state is LoginLoading) {
-              return SpinKitRotatingCircle(
-                color: Colors.sunset[20],
-                size: 50,
-              );
-            } else {
-              return SingleChildScrollView(
-                child: Align(
-                  alignment: Alignment.topCenter,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 16,
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Spacing.spacingV48,
-                        Text(
-                          'welcome_to'.tr,
-                          style: Headings.h4.copyWith(color: Colors.gray[90]),
-                        ),
-                        Spacing.spacingV24,
-                        const Text(
-                          'espacio para logo',
-                        ),
-                        Spacing.spacingV48,
-                        Input(
-                          controller: useernameMailController,
-                          hintText: 'username_mail'.tr,
-                          error:
-                              isErrorMail ? 'Ingresa un correo valido' : null,
-                          onChange: (String string) {
-                            if (!string.endsWith('@uniandes.edu.co')) {
-                              setState(() {
-                                isErrorMail = true;
-                              });
-                            } else {
-                              setState(() {
-                                isErrorMail = false;
-                              });
-                            }
-                            validateForm();
-                          },
-                        ),
-                        Spacing.spacingV16,
-                        Input(
-                          suffix: Icon(
-                            Icons.remove_red_eye,
-                            color: Colors.gray[80],
-                          ),
-                          controller: passwordControler,
-                          hintText: 'password'.tr,
-                          isPassword: true,
-                          error: isErrorPassword ? 'Campo obligatorio' : null,
-                          onChange: (String value) {
-                            setState(() {
-                              isErrorPassword = value.isEmpty;
-                            });
-                            validateForm();
-                          },
-                          maxLength: 16,
-                        ),
-                        Spacing.spacingV8,
-                        GestureDetector(
-                          onTap: () {
-                            Get.toNamed('/forgot_password');
-                          },
-                          child: Text(
-                            'forgot_your_password'.tr,
-                            style: TextStyle(
-                              color: Colors.gray[80],
-                              fontWeight: FontWeight.normal,
-                              fontSize: 14,
-                            ),
-                          ),
-                        ),
-                        Spacing.spacingV24,
-                        SunsetButton(
-                          text: 'log_in'.tr,
-                          onPressed: isValidForm ? signin : null,
-                          backgroundColor: !isValidForm
-                              ? Colors.gray[50]
-                              : Colors.sunset[50],
-                        ),
-                        Spacing.spacingV16,
-                        GestureDetector(
-                          onTap: () {
-                            Get.toNamed('/register');
-                          },
-                          child: RichText(
-                            text: TextSpan(
-                              children: <TextSpan>[
-                                TextSpan(
-                                  text: 'dont_have_an_account'.tr,
-                                  style: TextStyle(
-                                    color: Colors.gray[80],
-                                    fontSize: 16,
-                                  ),
-                                ),
-                                TextSpan(
-                                  text: 'Regístrate'.tr,
-                                  style: TextStyle(
-                                    color: Colors.gray[90],
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              );
+        body: BlocListener<LoginBloc, LoginState>(
+            bloc: loginBloc,
+          listener: (BuildContext context,LoginState state) {
+            if (state is LoginError) {
+              ScaffoldMessenger.of(context)
+                  ..hideCurrentSnackBar()
+                  ..showSnackBar(
+                    SnackBar(content: Text(state.errorMessage)),
+                  );
             }
           },
+          child: BlocBuilder<LoginBloc, LoginState>(
+            bloc: loginBloc,
+            builder: (BuildContext context, LoginState state) {
+              if (state is LoginLoading) {
+                return SpinKitRotatingCircle(
+                  color: Colors.sunset[20],
+                  size: 50,
+                );
+              } else {
+                return SingleChildScrollView(
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Spacing.spacingV48,
+                          Text(
+                            'welcome_to'.tr,
+                            style: Headings.h4.copyWith(color: Colors.gray[90]),
+                          ),
+                          Spacing.spacingV24,
+                          const Text(
+                            'espacio para logo',
+                          ),
+                          Spacing.spacingV48,
+                          Input(
+                            controller: useernameMailController,
+                            hintText: 'username_mail'.tr,
+                            error:
+                                isErrorMail ? 'Ingresa un correo valido' : null,
+                            onChange: (String string) {
+                              if (!string.endsWith('@uniandes.edu.co')) {
+                                setState(() {
+                                  isErrorMail = true;
+                                });
+                              } else {
+                                setState(() {
+                                  isErrorMail = false;
+                                });
+                              }
+                              validateForm();
+                            },
+                          ),
+                          Spacing.spacingV16,
+                          Input(
+                            suffix: Icon(
+                              Icons.remove_red_eye,
+                              color: Colors.gray[80],
+                            ),
+                            controller: passwordControler,
+                            hintText: 'password'.tr,
+                            isPassword: true,
+                            error: isErrorPassword ? 'Campo obligatorio' : null,
+                            onChange: (String value) {
+                              setState(() {
+                                isErrorPassword = value.isEmpty;
+                              });
+                              validateForm();
+                            },
+                            maxLength: 16,
+                          ),
+                          Spacing.spacingV8,
+                          GestureDetector(
+                            onTap: () {
+                              Get.toNamed('/forgot_password');
+                            },
+                            child: Text(
+                              'forgot_your_password'.tr,
+                              style: TextStyle(
+                                color: Colors.gray[80],
+                                fontWeight: FontWeight.normal,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ),
+                          Spacing.spacingV24,
+                          SunsetButton(
+                            text: 'log_in'.tr,
+                            onPressed: isValidForm ? signin : null,
+                            backgroundColor: !isValidForm
+                                ? Colors.gray[50]
+                                : Colors.sunset[50],
+                          ),
+                          Spacing.spacingV16,
+                          GestureDetector(
+                            onTap: () {
+                              Get.toNamed('/register');
+                            },
+                            child: RichText(
+                              text: TextSpan(
+                                children: <TextSpan>[
+                                  TextSpan(
+                                    text: 'dont_have_an_account'.tr,
+                                    style: TextStyle(
+                                      color: Colors.gray[80],
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: 'Regístrate'.tr,
+                                    style: TextStyle(
+                                      color: Colors.gray[90],
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              }
+            },
+          ),
         ),
       );
 }

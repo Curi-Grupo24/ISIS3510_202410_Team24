12fd1d102d1b958322537b856e48621535f024a9
Diana Alejandra Silva Alvarez <da.silvaa@uniandes.edu.co>
2024-04-11 23:06:53 -0500

    FEAT: added initial glimpses of the calendar views

--- lib/features/schedule/presentation/pages/pages.dart
+++ lib/features/schedule/presentation/pages/pages.dart
@@ -1,10 +1,13 @@
 library com.curi.app.schedule.pages;
 
+import 'dart:math' as math;
 import 'package:flutter/material.dart' hide Colors;
 import 'package:get/get.dart';
 
+import '../../../../core/constants/dates_constants.dart';
 import '../../../../core/ui/colors_brand.dart';
 import '../../../../core/ui/layout.dart';
+import '../../../class_flow/presentation/widgets/widgets.dart';
 
 part './schedule_home.dart';
 part './schedule_register.dart';
